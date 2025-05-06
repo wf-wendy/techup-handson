50c66c8d1db879c841726ef2d7f7bc5836f7bc80
wf-wendy <wfwf.work@gmail.com>
2025-05-06 09:08:03 +0000

    js stop at act3

---
+++ script.js
@@ -0,0 +1,19 @@
+var age=20
+age = 65
+
+var username = "preshant"
+var password = "techup"
+
+var isMale = true
+var isLoggedIn = false
+
+var blog_posts = ["Today was a good day", "The sky is blue", 
+    "It's raining heavily now"]
+
+console.log(age)
+
+console.log(username);
+console.log(password);
+console.log(isMale);
+console.log(isLoggedIn);
+console.log(blog_posts);
